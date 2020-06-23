11ccb53bc1c18ece695439866fcf4db70686c9b2
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-06-23 16:45:57 -0300

    first route

--- backend-meu-site/server.js
+++ backend-meu-site/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const server = express();
 
+
+server.get('/', (req, res) => {
+  return res.send('Rota index ok!');
+});
+
 server.listen(5000, () => {
   console.log("Server is Running!");
-})
+});
